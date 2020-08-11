99c6d6892fd912472497a561fbc919a8ad2c2bf7
tiantian <yanttde@outlook.com>
2020-08-11 12:32:40 +0800

    导入Vant组件库

--- src/main.js
+++ src/main.js
@@ -4,8 +4,17 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
+// 导入Vant组件库
+import Vant from "vant";
+// 导入Vant全局样式
+import "vant/lib/index.css";
+
 // 导入全局样式
 import "./style/index.scss";
+
+// 注册Vant总组件
+Vue.use(Vant);
+
 Vue.config.productionTip = false;
 
 new Vue({
